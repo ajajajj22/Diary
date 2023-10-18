376650f6802ade9f2ae7c5dd4f25f5ddcd7d5909
jeong03 <hjeong7367@gmail.com>
2023-10-18 09:03:08 +0900

    commit

--- diary/src/main/java/com/dakkoo/controller/BoardController.java
+++ diary/src/main/java/com/dakkoo/controller/BoardController.java
@@ -3,7 +3,7 @@ package com.example.Diary.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequestMapping("/demo")
+@RequestMapping("/diary")
 public class BoardController{
     @GetMapping("/login")
     public String saveForm(){
--- diary/src/main/java/com/dakkoo/controller/MemberController.java
+++ diary/src/main/java/com/dakkoo/controller/MemberController.java
@@ -3,7 +3,7 @@ package com.example.Diary.controller;
 @Controller
 public class MemberController {
     // 회원가입 페이지 출력 요청
-    @GetMapping("/member/register")
+    @GetMapping("/diart/register")
     public String registerForm() {
         return "register";
     }
