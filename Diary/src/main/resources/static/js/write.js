d9bbcddbbd458c87a428c6758068815fc9793a78
Gu Byeong Mo <kbmbaksa@naver.com>
2023-12-05 21:19:55 +0900

    2023_12_05 write

---
+++ Diary/src/main/resources/static/js/write.js
@@ -0,0 +1,128 @@
+document.getElementById('stickerButton').addEventListener('click', function() {
+    const fileInput = document.getElementById('imageInput');
+    const stickerContainer = document.getElementById('stickerContainer');
+
+    const imageFile = fileInput.files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+
+      const imageElement = document.createElement('img');
+      imageElement.src = event.target.result;
+
+      const stickerElement = document.createElement('div');
+      stickerElement.className = 'sticker';
+      stickerElement.appendChild(imageElement);
+
+      stickerContainer.appendChild(stickerElement);
+
+      makeStickerDraggable(stickerElement);
+    // 스티커 리사이즈 기능 추가
+    function makeStickerResizable(stickerElement) {
+        const resizeHandle = document.createElement('div');
+        resizeHandle.className = 'resize-handle';
+        stickerElement.appendChild(resizeHandle);
+    
+        let startX, startY, startWidth, startHeight;
+    
+        resizeHandle.addEventListener('mousedown', initResize, false);
+    
+        function initResize(e) {
+        e.preventDefault();
+        startX = e.clientX;
+        startY = e.clientY;
+        startWidth = parseInt(document.defaultView.getComputedStyle(stickerElement).width, 10);
+        startHeight = parseInt(document.defaultView.getComputedStyle(stickerElement).height, 10);
+        document.documentElement.addEventListener('mousemove', resize, false);
+        document.documentElement.addEventListener('mouseup', stopResize, false);
+        }
+    
+        function resize(e) {
+        const width = startWidth + e.clientX - startX;
+        const height = startHeight + e.clientY - startY;
+        stickerElement.style.width = width + 'px';
+        stickerElement.style.height = height + 'px';
+        }
+    
+        function stopResize() {
+        document.documentElement.removeEventListener('mousemove', resize, false);
+        document.documentElement.removeEventListener('mouseup', stopResize, false);
+        }
+    }
+  
+
+      stickerElement.style.width = '15px';
+      stickerElement.style.height = '7px';
+      stickerElement.style.position = 'absolute';
+      stickerElement.style.overflow = 'visible';
+      stickerElement.style.transform = 'scale(0.2)';
+
+    }
+
+    reader.readAsDataURL(imageFile);
+  });
+
+  function makeStickerDraggable(stickerElement) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    stickerElement.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      stickerElement.style.top = (stickerElement.offsetTop - pos2) + "px";
+      stickerElement.style.left = (stickerElement.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+  
+  // 스티커 리사이즈 기능 추가
+  function makeStickerResizable(stickerElement) {
+    const resizeHandle = document.createElement('div');
+    resizeHandle.className = 'resize-handle';
+    stickerElement.appendChild(resizeHandle);
+  
+    let startX, startY, startWidth, startHeight;
+  
+    resizeHandle.addEventListener('mousedown', initResize, false);
+  
+    function initResize(e) {
+      e.preventDefault();
+      startX = e.clientX;
+      startY = e.clientY;
+      startWidth = parseInt(document.defaultView.getComputedStyle(stickerElement).width, 10);
+      startHeight = parseInt(document.defaultView.getComputedStyle(stickerElement).height, 10);
+      document.documentElement.addEventListener('mousemove', resize, false);
+      document.documentElement.addEventListener('mouseup', stopResize, false);
+    }
+  
+    function resize(e) {
+      const width = startWidth + e.clientX - startX;
+      const height = startHeight + e.clientY - startY;
+      stickerElement.style.width = width + 'px';
+      stickerElement.style.height = height + 'px';
+    }
+  
+    function stopResize() {
+      document.documentElement.removeEventListener('mousemove', resize, false);
+      document.documentElement.removeEventListener('mouseup', stopResize, false);
+    }
+  }
+  
